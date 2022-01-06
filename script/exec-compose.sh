82dc5f3003862c26aeceed57552d1026227ddff0
sihandeng <sihan.deng@thoughtworks.com>
2022-01-06 09:38:16 +0800

    use docker-compose

--- script/exec-compose.sh
+++ script/exec-compose.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-#docker-compose down
-#docker-compose pull
-#docker-compose up -d
-
-docker run -d -p 8080:8080 2359721314/gocd-demo:v1
+docker-compose down
+docker-compose pull
+docker-compose up -d
